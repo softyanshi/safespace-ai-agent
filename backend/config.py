a5997141e9ad7ab9475aa36797c2d5529bd3d359
softyanshi <rt519552@gmail.com>
2025-09-20 00:38:33 +0530

    Add config

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+import os
+
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_FROM_NUMBER = os.environ.get("TWILIO_FROM_NUMBER")
+EMERGENCY_CONTACT = os.environ.get("EMERGENCY_CONTACT")
+
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
